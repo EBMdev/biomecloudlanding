45ed339b921b8b1efc2fe3774ba1d92d5e7893c3
EBM <126194720+EBMdev@users.noreply.github.com>
2026-06-24 11:45:25 +0200

    Add site and base configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://ebmdev.github.io',
+  base: '/biomecloudlanding/',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
